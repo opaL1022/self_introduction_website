9ec8d3f452830befc0e70b582ee311bfcc2e4019
Max7414 <90671891+Max7414@users.noreply.github.com>
2025-06-16 19:32:15 +0800

    add files

---
+++ src/app/max/page.tsx
@@ -0,0 +1,7 @@
+// src/app/testrouter/page.tsx
+
+import Terminal from '@/components/Terminal';  // 或 '../../components/Terminal'
+
+export default function MaxRouterPage() {
+  return <Terminal />;
+}
